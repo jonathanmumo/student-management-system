9728f9b2c4d91cb1a13e6a009bd4c17aff2bac82
Jonathan mumo <zebedeealubisia374@gmail.com>
2026-07-01 16:16:42 +0300

    Add initial Pascal source code

---
+++ main.pas
@@ -0,0 +1,18 @@
+program StudentManagementSystem;
+
+uses crt;
+
+begin
+    clrscr;
+    writeln('==============================');
+    writeln(' STUDENT MANAGEMENT SYSTEM');
+    writeln('==============================');
+    writeln;
+    writeln('1. Add Student');
+    writeln('2. View Students');
+    writeln('3. Calculate Grades');
+    writeln('4. Generate Report');
+    writeln;
+    writeln('System Running...');
+    readln;
+end.
